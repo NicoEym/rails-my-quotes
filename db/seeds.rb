c9e4b3e0d17580974780268ea51ebfc0b0aef5eb
nicolas <nicolas.eymenier@gmail.com>
2019-04-12 23:57:20 +0200

    refacto seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,25 +7,39 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 require "open-uri"
 require "nokogiri"
+require 'awesome_print'
 
-
-def scrape_investing(url)
+def scrape_investing(url, assets, asset_type)
   doc = Nokogiri::HTML(open(url).read)
-  # p idx = doc.search(".c-list-trading__item--name")
-  # puts "Price"
-  # doc.search(".c-instrument--last").each do |price|
-  #   p price.text
-  # end
-  # puts "Variation"
-  # p var = doc.search(".c-instrument--variation").class
-  asset = Asset.new(asset_name: "CAC 40")
-  asset.last_price = doc.search("#pair_167 .pid-167-last").text.to_f
-  asset.daily_variation = doc.search("#pair_167 .pid-167-pcp").text.to_f
-  asset.save
 
+  puts "Stocks indices"
+
+  assets.each do |asset|
+    asset = Asset.new(asset_name: asset["name"])
+    puts doc.search("#pair_#{asset["investing_id"]} .pid-#{asset["investing_id"]}-last").text.to_f
+    puts doc.search("#pair_#{asset["investing_id"]} .pid-#{asset["investing_id"]}-pcp").text.to_f
+    asset.save
+  end
 end
 
-scrape_investing("https://fr.investing.com/indices/major-indices")
+ref_equity = [{ "name" => 'CAC 40', "investing_id" => 167 },
+          { "name" => 'Eurostoxx 50', "investing_id" => 175 },
+          { "name" => 'S&P500', "investing_id" => 166 },
+          { "name" => 'Nasdaq', "investing_id" => 14958 },
+          { "name" => 'VIX', "investing_id" => 44336 },
+          { "name" => 'Ibovespa', "investing_id" => 17920 },
+          { "name" => 'DAX', "investing_id" => 172 },
+          { "name" => 'FTSE 100', "investing_id" => 27 },
+          { "name" => 'Nikkei 225', "investing_id" => 178 },
+          { "name" => 'Hang Seng', "investing_id" => 179 },
+        ]
+
+scrape_investing("https://fr.investing.com/indices/major-indices", ref_equity, "Equity")
+
+
+
+
+
 
 
 
