d2376e3d3a7a66008a5c9aabe8e2bad702b50bbc
nicolas <nicolas.eymenier@gmail.com>
2019-04-13 22:38:27 +0200

    references quotes

--- db/seeds.rb
+++ db/seeds.rb
@@ -31,15 +31,53 @@ ref_equity = [{ "name" => 'CAC 40', "investing_id" => 167 },
           { "name" => 'DAX', "investing_id" => 172 },
           { "name" => 'FTSE 100', "investing_id" => 27 },
           { "name" => 'Nikkei 225', "investing_id" => 178 },
-          { "name" => 'Hang Seng', "investing_id" => 179 },
+          { "name" => 'Hang Seng', "investing_id" => 179 }
         ]
 
 scrape_investing("https://fr.investing.com/indices/major-indices", ref_equity, "Equity")
 
 
 
+ref_forex = [{ "name" => 'EUR/USD', "investing_id" => 1 },
+          { "name" => 'USD/BRL', "investing_id" => 2103 },
+          { "name" => 'USD/JPY', "investing_id" => 3 },
+          { "name" => 'GBP/USD', "investing_id" => 2 },
+          { "name" => 'USD/CHF', "investing_id" => 4 }
+        ]
+
+scrape_investing("https://fr.investing.com/currencies/single-currency-crosses", ref_forex, "Equity")
 
 
+ref_sovereign_10Y = [{ "name" => 'Germany', "investing_id" => 23693 },
+          { "name" => 'USA', "investing_id" => 23705 },
+          { "name" => 'Brazil', "investing_id" => 24029 },
+          { "name" => 'France', "investing_id" => 23778 },
+          { "name" => 'Italy', "investing_id" => 23738 },
+          { "name" => 'UK', "investing_id" => 23673 },
+          { "name" => 'Spain', "investing_id" => 23806 }
+        ]
+
+scrape_investing("https://fr.investing.com/rates-bonds/world-government-bonds", ref_sovereign_10Y, "Equity")
+
+
+ref_commodities = [{ "name" => 'Gold', "investing_id" => 23693 },
+          { "name" => 'Silver', "investing_id" => 23705 },
+          { "name" => 'WTI', "investing_id" => 24029 },
+          { "name" => 'Brent', "investing_id" => 23778 }
+        ]
+
+scrape_investing("https://fr.investing.com/commodities/real-time-futures", ref_commodities, "Equity")
+
+
+
+ref_crypto = [{ "name" => 'BTC/USD', "investing_id" => 1073245 },
+          { "name" => 'ETH/USD', "investing_id" => 1031703 },
+          { "name" => 'XRP/USD', "investing_id" => 1057990 },
+          { "name" => 'BCH/USD', "investing_id" => 1058255 },
+          { "name" => 'LTC/USD', "investing_id" => 1031704 }
+        ]
+
+scrape_investing("https://fr.investing.com/crypto/currency-pairs", ref_commodities, "Equity")
 
 
 
