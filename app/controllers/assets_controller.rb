687d7d054a752fa21a8098d225a2e7bdc822b691
nicolas <nicolas.eymenier@gmail.com>
2019-04-23 22:19:07 +0200

    removed controller assets

--- app/controllers/assets_controller.rb
+++ app/controllers/assets_controller.rb
@@ -1,18 +1,1 @@
-class AssetsController < ApplicationController
 
-  def new
-    asset = Asset.new
-  end
-
-  def create
-    asset = Asset.new(params[:asset])
-    asset.save
-  end
-
-  private
-
-  def asset_params
-    params.require(:asset).permit(:asset_name, :last_price, :daily_variation)
-  end
-
-end
